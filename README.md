52881894b450fe35f6c360d00e9e74fe0176551f
Ayman Ait Laaguid <laaguid.ayman@gmail.com>
2023-07-12 13:16:41 +0100

    NavBar

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # **Changes**
 
 - Set up project structure/configuration files & Started constructig the Home page
+- Created a responsive NavBar & set up a skeleton for the client side authentication
--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ function Home() {
 		<section className="w-full flex-center flex-col">
 			<h1 className="head_text text-center">
 				Discover & Share
-				<br className="max-md:hidden" />
+				<br className="" />
 				<span className="red_gradient text-center">AI-Powered Prompts</span>
 				<p className="desc text-center">
 					Prompy is an open-source AI prompting tool for modern world to
